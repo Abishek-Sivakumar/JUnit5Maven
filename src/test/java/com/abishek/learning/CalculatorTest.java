6e5d43899104dd1a5bb5eac56ddcf255fa68df68
ABISHEK <abideepu19.vrs@gmail.com>
2025-01-28 21:25:37 +0530

    Before and After Methods along with TestInstance

--- src/test/java/com/abishek/learning/CalculatorTest.java
+++ src/test/java/com/abishek/learning/CalculatorTest.java
@@ -1,9 +1,12 @@
 package com.abishek.learning;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class CalculatorTest {
 //    @Test
 //    void twoPlusTwoEquals4(){
@@ -19,17 +22,44 @@ class CalculatorTest {
 //                });
 //    }
 
+
+    Calculator calc;
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Before all the tests");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("After all the tests");
+    }
+
+    @BeforeEach
+    void initCalc(){
+       calc = new Calculator();
+        System.out.println("Before performing each Unit Test");
+    }
+
+    @AfterEach
+    void exitCalc(){
+        System.out.println("All tests were run");
+    }
+
     @Test
     void sumEqualsFive(){
         var calc= new Calculator();
-        assertEquals(6,calc.add(2,3),"Just Kidding the sum of 2 and 3 is 5");
+//        assertEquals(6,calc.add(2,3),"Just Kidding the sum of 2 and 3 is 5");
+        assertTimeout(Duration.ofMillis(100),()->calc.add(2,3));
+        System.out.println("End of test");
     }
 
    @Test
     void checkArrays(){
        int[] arr1 = {1,2,3,4};
-       int[] arr2 = {3,2,1,4};
+       int[] arr2 = {1,2,3,4};
        assertArrayEquals(arr1,arr2);
+       System.out.println("End of test");
    }
 
 }
